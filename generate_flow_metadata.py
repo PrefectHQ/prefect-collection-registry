ce83a33ddbce3d15347aecc6ac43997107e8bfa8
Nathan Nowack <thrast36@gmail.com>
2023-02-06 22:38:31 -0600

    add flow generation script

--- generate_flow_metadata.py
+++ generate_flow_metadata.py
@@ -1,4 +1,4 @@
-import inspect, json, subprocess
+import github3, inspect, json, os
 from collections import defaultdict
 from griffe.dataclasses import Docstring
 from griffe.docstrings.parsers import Parser, parse
@@ -6,22 +6,24 @@ from pkgutil import iter_modules
 from prefect import Flow, flow
 from prefect.utilities.importtools import load_module
 from typing import Any, Dict, Generator
-from utils import get_collection_names, skip_parsing
+from utils import skip_parsing
+
+skip_sections = {"parameters", "raises"}
 
 
 def parse_flow_docstring(flow: Flow) -> Dict[str, Any]:
     """
     Parses the docstring of a flow.
-    """    
+    """
     sections = parse(Docstring(flow.description), Parser.google)
-    
+
     # created serialized version of each docstring section
     docstring_sections = defaultdict(list)
-    
+
     for section in sections:
         section = section.as_dict()
         section_value = section.get("value", [])
-        if section["kind"] in {"parameters", "raises"}:
+        if section["kind"] in skip_sections:
             continue
         elif section["kind"] == "text":
             docstring_sections["summary"] = section_value
@@ -29,40 +31,41 @@ def parse_flow_docstring(flow: Flow) -> Dict[str, Any]:
             docstring_sections[section["kind"]] = "\n".join(
                 return_obj.description for return_obj in section_value
             )
-        elif section["kind"] == "admonition" and section_value["annotation"] == "example":
+        elif (
+            section["kind"] == "admonition" and section_value["annotation"] == "example"
+        ):
 
             docstring_sections["examples"].append(section_value["description"])
 
-            
     return docstring_sections
 
+
 def summarize_flow(flow: Flow, collection_name: str):
-    
-    install_command = f"pip install {collection_name.replace('_', '-')}"
-    
     return {
         "name": flow.name,
         "parameters": dict(flow.parameters),
-        "install_command": install_command,
-        "description": {**parse_flow_docstring(flow)}
+        "install_command": f"pip install {collection_name.replace('_', '-')}",
+        "description": {**parse_flow_docstring(flow)},
     }
 
 
 def find_flows_in_module(
     module_name: str,
-    collection_name: str = None,
 ) -> Generator[Flow, None, None]:
     """
     Finds all flows in a module.
     """
     module = load_module(module_name)
-    
-    if collection_name is None:
-        collection_name = module_name
+
+    first = True
+
+    if first:
+        first = False
+        collection_name = module.__name__
 
     for _, name, ispkg in iter_modules(module.__path__):
         if ispkg:
-            
+
             yield from find_flows_in_module(f"{module_name}.{name}", collection_name)
         else:
             try:
@@ -70,7 +73,7 @@ def find_flows_in_module(
                 print(f"\tLoaded submodule {module_name}.{name}...")
             except ModuleNotFoundError:
                 continue
-            
+
             for _, obj in inspect.getmembers(submodule):
                 if skip_parsing(name, obj, module_name):
                     continue
@@ -78,29 +81,54 @@ def find_flows_in_module(
                     yield obj
 
 
+# @flow
+def generate_flow_metadata(collection_name: str):
+    """
+    Generates a JSON file containing metadata about all flows in a given collection.
+
+    Creates or updates a PR with a commit containing the updated JSON file.
+    """
+    collection_slug = collection_name.replace("-", "_")
+    flow_metadata_file = "collections/collection_flows_metadata.json"
+    BRANCH_NAME = "flow-metadata"
+
+    # read the existing flow metadata from existing JSON file
+    # within the collection repo
+    gh = github3.login(token=os.getenv("GITHUB_TOKEN"))
+
+    prefect_core_repo = gh.repository("PrefectHQ", "prefect")
+    registry_repo = gh.repository("PrefectHQ", "prefect-collection-registry")
+    collection_repo = gh.repository("PrefectHQ", f"{collection_name}")
+    latest_release = collection_repo.latest_release().tag_name
+
+    print(f"found {collection_repo} {latest_release}")
+
+    flow_metadata_raw = registry_repo.file_contents(
+        flow_metadata_file, ref="flow-metadata"
+    ).decoded.decode()
+
+    # make a dict of the existing flow metadata
+    flow_metadata = json.loads(flow_metadata_raw)
+
+    collection_module = load_module(collection_slug)
+
+    print(f"Loaded collection {collection_module.__name__}...")
+
+    collection_flow_metadata = {
+        flow.name: summarize_flow(flow, collection_slug)
+        for flow in find_flows_in_module(collection_slug)
+    }
+    if collection_flow_metadata:
+        flow_metadata.update({collection_name: collection_flow_metadata})
+    
+        # create a new commit with the updated flow metadata
+        registry_repo.file_contents(
+            flow_metadata_file, ref=BRANCH_NAME
+        ).update(
+            message=f"Update flow metadata for {collection_name} {latest_release}",
+            content=json.dumps(flow_metadata, indent=4).encode("utf-8"),
+            branch=BRANCH_NAME,
+        )
+
 if __name__ == "__main__":
-
-    flow_metadata = {}
-
-    for collection_name in get_collection_names():
-        print(f"Loading collection {collection_name}...")
-
-        try:
-            collection_module = load_module(collection_name)
-        except ModuleNotFoundError:
-            subprocess.run(["pip", "install", collection_name])
-            collection_module = load_module(collection_name)
-
-        collection_flow_metadata = {
-            flow.name: summarize_flow(flow, collection_name)
-            for flow in find_flows_in_module(collection_name)
-        }
-        if collection_flow_metadata:
-            flow_metadata.update(
-                {
-                    collection_name.replace("_", "-") :
-                    collection_flow_metadata
-                }
-            )
-    with open("collections/collection_flows_metadata.json", "w") as f:
-        json.dump(flow_metadata, f, indent=4)
+    generate_flow_metadata("prefect-airbyte")
