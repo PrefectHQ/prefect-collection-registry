ec3d2f73c9bd9704ed359dc6942119766b1c5c0c
Alexander Streed <alex.s@prefect.io>
2023-02-13 09:05:16 -0600

    Moves current collection block metadata files

--- src/merge_block_metadata.py
+++ src/merge_block_metadata.py
@@ -5,9 +5,14 @@ if __name__ == "__main__":
     collection_dirs = Path("collections").glob("prefect-*")
     collections = {"collections": {}}
     for collection_dir in sorted(collection_dirs):
+        (collection_dir / "blocks").mkdir(exist_ok=True)
         # get the latest tag
-        path = sorted(collection_dir.glob("*.json"), reverse=True)[0]
-        collections["collections"][collection_dir.stem] = json.loads(path.read_text())
+        matches = sorted((collection_dir / "blocks").glob("*.json"), reverse=True)
+        if matches:
+            path = matches[0]
+            collections["collections"][collection_dir.stem] = json.loads(
+                path.read_text()
+            )
     (Path("views") / "aggregate-block-metadata.json").write_text(
         json.dumps(collections, indent=2)
     )
