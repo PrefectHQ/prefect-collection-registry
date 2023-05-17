8ba2df16a6f1092caf2dc7703bef28f6321d4386
Alexander Streed <alex.s@prefect.io>
2023-05-17 13:20:29 -0500

    Adds code for new fields

--- src/generate_worker_metadata.py
+++ src/generate_worker_metadata.py
@@ -29,11 +29,15 @@ def generate_worker_metadata(worker_subcls: Type[BaseWorker], package_name: str)
                 "type": worker_subcls.type,
                 "install_command": f"pip install {package_name}",
                 "description": worker_subcls.get_description(),
+                "display_name": getattr(
+                    worker_subcls, "display_name", worker_subcls.type
+                ),
                 "logo_url": worker_subcls.get_logo_url(),
                 "documentation_url": worker_subcls.get_documentation_url(),
                 "default_base_job_configuration": (
                     worker_subcls.get_default_base_job_template()
                 ),  # noqa E501
+                "is_beta": getattr(worker_subcls, "is_beta", False),
             }.items()
         )
     )
