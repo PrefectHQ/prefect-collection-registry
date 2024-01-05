eedd5e14d0f6142f052575c8b402c131b5d95fad
Nathan Nowack <thrast36@gmail.com>
2024-01-05 13:05:07 -0600

    fix auth

--- src/pr_views_to_core.py
+++ src/pr_views_to_core.py
@@ -4,10 +4,8 @@ from datetime import datetime
 from prefect.blocks.system import Secret
 
 GITHUB_TOKEN = Secret.load("collection-registry-contents-prs-rw-pat").get()
-os.environ['GITHUB_TOKEN'] = GITHUB_TOKEN
-
 SOURCE_REPO_URL = 'https://raw.githubusercontent.com/PrefectHQ/prefect-collection-registry/main/views/aggregate-worker-metadata.json'
-TARGET_FILE_PATH = 'src/prefect/server/api/collections_data/views/aggregate-worker-metadata.json' # noqa: E501
+TARGET_FILE_PATH = 'src/prefect/server/api/collections_data/views/aggregate-worker-metadata.json'
 TARGET_ORG = 'PrefectHQ'
 TARGET_REPO = 'prefect'
 NEW_BRANCH = f'update-worker-metadata-{datetime.now().strftime("%Y%m%d%H%M%S")}'
@@ -19,6 +17,7 @@ commands = f"""
     curl -o {TARGET_FILE_PATH} {SOURCE_REPO_URL} &&
     git config user.email "marvin@prefect.io" &&
     git config user.name "marvin-robot" &&
+    git remote set-url origin https://x-access-token:{GITHUB_TOKEN}@github.com/{TARGET_ORG}/{TARGET_REPO}.git &&
     git checkout -b {NEW_BRANCH} &&
     git add {TARGET_FILE_PATH} &&
     git commit -m "Update aggregate-worker-metadata.json" &&
